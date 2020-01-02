3c2a359bbcd94e0ccc59c5036d2da99024454f0a
KID <50666207+KID421@users.noreply.github.com>
2020-01-02 20:13:26 +0800

    隨機技能

--- UnityProject/Assets/Scripts/RandomSkill.cs
+++ UnityProject/Assets/Scripts/RandomSkill.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;   // 引用介面 API
+using UnityEngine.UI;       // 引用介面 API
+using System.Collections;
 
 namespace KID
 {
@@ -8,26 +9,66 @@ namespace KID
         [Header("技能圖片：模糊與正常")]
         public Sprite[] spriteBlurs;        // 模糊圖片陣列 16
         public Sprite[] spriteSkills;       // 技能圖片陣列 8
+        [Header("捲動速度"), Range(0.00001f, 3f)]
+        public float speed = 0.1f;
+        [Header("捲動次數"), Range(1, 10)]
+        public int count = 5;
+        [Header("音效")]
+        public AudioClip soundScroll;
+        public AudioClip soundGetSkill;
 
+        private string[] nameSkills = { "連續射擊", "正向箭", "背向箭", "側向箭", "血量提升", "攻擊提升", "攻速提升", "爆擊提升" };
         private Image img;                  // 圖片元件
         private Button btn;                 // 按鈕元件
+        private AudioSource aud;            // 音源元件
+        private Text textSkill;             // 技能名稱
+        private GameObject panelSkill;      // 隨機技能物件
 
         private void Start()
         {
             img = GetComponent<Image>();
             btn = GetComponent<Button>();
-            // 啟動協程
+            aud = GetComponent<AudioSource>();
+            textSkill = transform.GetChild(0).GetComponent<Text>();     // 變形.取得子物件(編號)
+            panelSkill = GameObject.Find("隨機技能");
+
+            btn.onClick.AddListener(ChooseSkill);                       // 按鈕.點擊.增加監聽者(選擇技能)
+
+            StartCoroutine(ScrollEffect());                             // 啟動協程
         }
 
-        // 定義協程方法 捲動效果
+        /// <summary>
+        /// 選擇技能後：隱藏隨機技能物件
+        /// </summary>
+        private void ChooseSkill()
+        {
+            panelSkill.SetActive(false);                                // 隱藏隨機技能物件
+        }
 
-        // 按鈕無法點選
+        /// <summary>
+        /// 捲動效果
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator ScrollEffect()
+        {
+            btn.interactable = false;                           // 按鈕無法點選
 
-        // 迴圈
-        // 圖片元件.圖片 = 模糊圖片陣列[編號]
-        
-        // 隨機挑選 技能圖片陣列
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < spriteBlurs.Length; i++)
+                {
+                    img.sprite = spriteBlurs[i];                // 圖片元件.圖片 = 模糊圖片陣列[編號]
+                    aud.PlayOneShot(soundScroll, 0.2f);
+                    yield return new WaitForSeconds(speed);     // 等待
+                }
+            }
 
-        // 按鈕可以點選
+            int r = Random.Range(0, spriteSkills.Length);       // 隨機值 = 隨機(最小值，最大值)
+            img.sprite = spriteSkills[r];                       // 圖片元件.圖片 = 技能圖片陣列[隨機值]
+            aud.PlayOneShot(soundGetSkill, 0.8f);
+            textSkill.text = nameSkills[r];                     // 技能名稱.文字 = 技能名稱[隨機值]
+
+            btn.interactable = true;                            // 按鈕可以點選
+        }
     }
 }
